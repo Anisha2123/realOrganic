b84682ef53391cf8a39bae34f4e860c56771852a
Anisha2123 <birlaani@gmail.com>
2026-03-02 23:30:47 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,33 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+// const allowedOrigins = process.env.CORS_ORIGIN.split(",");
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// };
+
 const allowedOrigins = process.env.CORS_ORIGIN.split(",");
 
-const corsOptions = {
+app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+    if (!origin) return callback(null, true); // allow server-to-server
+
+    if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true,
-};
+  credentials: true
+}));
 
 
 app.use(cors(corsOptions));
